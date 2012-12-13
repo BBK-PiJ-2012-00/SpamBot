4b7651ce2135007006aee93fd38bbd353e61dd3f
BBK-PiJ-2012-00 <ataylo20@dcs-bbk-ac-uk>
2012-12-13 11:32:14 +0000

    Complete :D

--- PageReader.java
+++ PageReader.java
@@ -1,53 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spambot;
 
-
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 /**
  *
  * @author tnealo01
  */
-public class PageReader {
+public class PageReader implements WebPage {
     // Not implemented now returns as many links as it can find.
-    private static int linksCount = 0;
+    //private static int maxLinks = 50;
     private String url;
     // Current list of lines from the webpage.
-    private List<String> pageLines = new ArrayList<String>();
+    private List<String> pageLines = new ArrayList<>();
+    static private int counter;
+    
+    
+    public static int getCount() {
+        return counter;
+    }
     
     public PageReader(String url) {
         this.url = url;
-        linksCount++;
+        counter += 1;
     }
             
+    @Override
     public String getUrl() {
         return url;
     }
     
-    public static int getCount() {
-    	return linksCount;
-    }
-    
+    @Override
     public Set<String> getLinks() {
         if (pageLines.isEmpty()) {
             pageLines = getPage();
         } 
-        Set<String> myLinkSet = new HashSet<String>(getListLinks(pageLines));
+        Set<String> myLinkSet = new HashSet<>(getListLinks(pageLines));
+        ///////
+        System.out.println();
+        //System.out.println("Gathered emails");
+//        Iterator<String> emailIterator = myLinkSet.iterator();
+//        while (emailIterator.hasNext()) {
+//            System.out.println("Checking:::: " + emailIterator.next());
+//        }
         return myLinkSet;
     }
     
+    @Override
     public Set<String> getEmails() {
         if (pageLines.isEmpty()) {
             pageLines = getPage();
         } 
-        Set<String> myEmailSet = new HashSet<String>(getListEmails(pageLines));
+        Set<String> myEmailSet = new HashSet<>(getListEmails(pageLines));
         return myEmailSet;
     }
     
     private List<String> getPage() {
         // Get the page and return a nice list of lines
         // for use by the link getter and email getter methods.
-        List<String> lines = new ArrayList<String>();
+        List<String> lines = new ArrayList<>();
         BufferedReader bufferedReader;
         try {
             bufferedReader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
@@ -67,7 +86,7 @@ public class PageReader {
     
     private List<String> getListLinks(List<String> lines) {
         String result;
-        List<String> links = new ArrayList<String>();
+        List<String> links = new ArrayList<>();
         String linkRegex = "href=\"[^>]*\">";
         for(String each : lines) {
             try {
@@ -83,8 +102,10 @@ public class PageReader {
                         result = result.replaceFirst("&amp.*", "");
                         // Still does not append http://domain to the links without it or
                         // pass over emailto: and javascript: links.
-                        links.add(result);
-                        System.out.println(result);
+                        if (result.startsWith("https://") || result.startsWith("http://") || result.startsWith("www.")) {
+                            links.add(result);
+                            //System.out.println(result);
+                        }
                     }
                 } while(result != null);
             } catch (NullPointerException e) {
@@ -96,7 +117,7 @@ public class PageReader {
     }
     
     private List<String> getListEmails(List<String> lines) {
-        List<String> emails = new ArrayList<String>();
+        List<String> emails = new ArrayList<>();
         String result;
         String mailRegex = "([_A-Za-z0-9-]+)(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})";
         for(String each : lines) {
@@ -106,7 +127,7 @@ public class PageReader {
                     result = myScanner.findInLine(mailRegex);
                     if (result != null) {
                         emails.add(result);
-                        System.out.println(result);
+                        //System.out.println(result);
                     }
                 } while(result != null);
             } catch (NullPointerException e) {
